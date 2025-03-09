a8d8142a400f9c9d3716f50c06a64c8fa2a399f0
Junior-81 <jab.junior81@gmail.com>
2025-03-09 11:58:02 -0300

    atualização banco.java

--- src/Banco.java
+++ src/Banco.java
@@ -1,24 +1,51 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
 
-	private String nome;
-	private List<Conta> contas;
+    private String nome;
+    private List<Conta> contas;
 
-	public String getNome() {
-		return nome;
-	}
+    public Banco(String nome) {
+        this.nome = nome;
+        this.contas = new ArrayList<>();
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public List<Conta> getContas() {
-		return contas;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setContas(List<Conta> contas) {
-		this.contas = contas;
-	}
+    public List<Conta> getContas() {
+        return contas;
+    }
 
+    public void adicionarConta(Conta conta) {
+        if (conta != null) {
+            contas.add(conta);
+            System.out.println("Conta adicionada com sucesso!");
+        } else {
+            System.out.println("Erro: Conta inválida!");
+        }
+    }
+
+    public void listarContas() {
+        System.out.println("\nLista de Contas no Banco " + nome + ":");
+        for (Conta conta : contas) {
+            System.out.println("Número: " + conta.getNumero() + " | Titular: " + conta.cliente.getNome() + " | Saldo: R$ " + conta.getSaldo());
+        }
+    }
+
+    public Conta buscarContaPorNumero(int numero) {
+        for (Conta conta : contas) {
+            if (conta.getNumero() == numero) {
+                return conta;
+            }
+        }
+        System.out.println("Conta não encontrada!");
+        return null;
+    }
 }
